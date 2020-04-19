a66abebe54d3871b329288a19cb4f7cbcda26ae4
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2020-04-19 16:52:13 -0500

    added cors

--- node-mongo-auth/server.js
+++ node-mongo-auth/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express");
+var cors = require('cors');
 const BodyParser =  require("body-parser");
 const mongoose = require('mongoose');
 const User =  require("./Models/User");
@@ -8,6 +9,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(cors());
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({ extended: true }));
 
