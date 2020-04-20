f45e69077d3e83a900ac92a8ffd5130457022ce6
Kaustubh Ghadge <kaustubhghadge@outlook.com>
2020-04-19 21:17:24 -0500

    load in table

--- js-intermediate-covid19/main.js
+++ js-intermediate-covid19/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-$('.btn').click(function() {
-  
-  $('.text').text('loading . . .');
-  
+$('.btn').click(function() {  
   $.ajax({
     type:"GET",
     contentType: "application/json",
     url:"https://api.covid19api.com/summary",
     success: function(data) {
-      $('.text').text(JSON.stringify(data));
+      var trHTML='';
+      var countries = [];
+      countries = data.Countries; 
+      Object.values(countries).forEach(country => {
+        trHTML += '<tr><td><strong>' + country.Country+'</strong></td><td><span class="label label-success">'+country.TotalConfirmed +'</span></td></tr>'
+      });
+      $('#country-list').html(trHTML); 
     },
   });
-  
 });
 });
